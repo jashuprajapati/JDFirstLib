3436c60e80a5338fb23bc5549d3dd2665d9ad8a1
Jashu_Prajapati <jashu@setblue.com>
2021-05-03 21:37:08 +0530

    Initial project setup

---
+++ JDFirstLibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JDFirstLibExample
+//
+//  Created by Apple on 03/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
