f5ebac9f646744a6a75e6fc4a83522a317853869
Jashu_Prajapati <jashu@setblue.com>
2021-05-03 21:22:11 +0530

    Initial Commit

---
+++ JDFirstLib/JDFirstLib.h
@@ -0,0 +1,18 @@
+//
+//  JDFirstLib.h
+//  JDFirstLib
+//
+//  Created by Apple on 03/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JDFirstLib.
+FOUNDATION_EXPORT double JDFirstLibVersionNumber;
+
+//! Project version string for JDFirstLib.
+FOUNDATION_EXPORT const unsigned char JDFirstLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDFirstLib/PublicHeader.h>
+
+
